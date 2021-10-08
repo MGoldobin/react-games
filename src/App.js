93eeab2cb96980af305794e91fe9af64494b9981
MGoldobin <misha2000goldobin@mail.ru>
2021-10-08 23:57:52 +0500

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Game from './components/Game'
+import Game from './components/XOX/Game.jsx'
+import Main from './components/Main.jsx'
+import e404 from './components/e404.jsx'
 import React from 'react'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Game />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Main}/>
+        <Route path="/xox" component={Game}/>
+        <Route path="/404" component={e404}/>
+        <Redirect to="/404" />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
