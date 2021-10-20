28f2bec027fd562f248548519e990e30c51fcdba
MGoldobin <misha2000goldobin@mail.ru>
2021-10-20 20:33:27 +0500

    Update Game.jsx

--- src/components/XOX/Game.jsx
+++ src/components/XOX/Game.jsx
@@ -1,6 +1,6 @@
 ﻿import React, { useState } from 'react'
 import Board from './Board.jsx'
-import Select from '../UI/Select.jsx'
+import Select from '../../UI/Select.jsx'
 import '../../style/XOX/Game.css'
 import whoWinner from '../../vendor/winner.js'
 import Popup from './Popup.jsx'
