1f21a36bacf7283559786c3837d6bf89662a7c55
MGoldobin <misha2000goldobin@mail.ru>
2021-10-20 20:04:06 +0500

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../style/Footer.css'
+
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<a className="footer__link" href="https://github.com/MGoldobin" target="blank">
+				<img className="footer__img" src="./GitHub.png" alt="GitHub" />
+			</a>
+		</footer>
+	)
+}
+
+export default Footer
