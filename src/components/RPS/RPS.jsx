44368ee6db7bd6b04853dfebd47f69b8323be04a
MGoldobin <misha2000goldobin@mail.ru>
2021-10-22 01:57:40 +0500

    add game logic

--- src/components/RPS/RPS.jsx
+++ src/components/RPS/RPS.jsx
@@ -1,7 +1,9 @@
-﻿import React/*, { useState }*/ from 'react'
+import React, { useState } from 'react'
 import '../../style/RPS/RPS.css'
 import { NavLink } from 'react-router-dom'
 import Smile from './Smile'
+import Popup from '../UI/Popup'
+import whoWinner from '../../vendor/winnerRPS'
 
 const smiles = [
 	{ name: "Rock", url: "./rock.png" },
@@ -11,15 +13,18 @@ const smiles = [
 
 const Rps = () => {
 	document.title = "RPS"
-	//const [computerResult, setComputerResult] = useState({ name: "...", url: "./undefined.png" })
-	//const [playerResult, setPlayerResult] = useState({ name: "Rock", url: "./rock.png" })
+	const [computerResult, setComputerResult] = useState({ name: "...", url: "./undefined.png" })
+	const [playerResult, setPlayerResult] = useState({ name: "Rock", url: "./rock.png" })
+	let rand = 0
 
-	const chooseAndGame = (name, url) => {
-		console.log(name, url)
-		/*
-		setPlayerResult(name, url)
-		const rand = Math.floor(Math.random() * 3)
-		setComputerResult(smiles[rand].name, smiles[rand].url)*/
+	const startNewGame = () => {
+		setComputerResult({ name: "...", url: "./undefined.png" })
+	}
+
+	const handleClick = (name, url) => {
+		rand = Math.floor(Math.random() * 3)
+		setComputerResult({ name: smiles[rand].name, url: smiles[rand].url })
+		setPlayerResult({ name: name, url: url })
 	}
 
 	return (
@@ -31,23 +36,31 @@ const Rps = () => {
 			<ul className="rps__rules">
 				<li className="rps__rule">The rock beats the scissors</li>
 				<li className="rps__rule">The scissors beats the paper</li>
-				<li className="rps__rule">The paper beats the rock</li>
+				<li className="rps__rule" >The paper beats the rock</li>
 			</ul>
 			<div className="rps__board">
 				<div className="rps__gameField">
 					{
 						smiles.map(smile => (
 							<Smile
+								handleClick={handleClick}
 								key={smile.name}
 								name={smile.name}
 								url={smile.url}
-								onClick={chooseAndGame(smile.name, smile.url)}
+								disabled={null}
 							/>
 						))
 					}
 				</div>
+				{
+					whoWinner(playerResult.name, computerResult.name) ? <Popup result={whoWinner(playerResult.name, computerResult.name)} startNewGame={startNewGame} /> : null
+				}
 				<div className="rps__gameField">
-					<Smile name="..." url="./undefined.png" />
+					<Smile
+						name={computerResult.name}
+						url={computerResult.url}
+						disabled="none"
+					/>
 				</div>
 			</div>
 		</div>
