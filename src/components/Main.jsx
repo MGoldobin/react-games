6a0ed8e14356f69b5d514e98ba1fa7599b9bd773
MGoldobin <misha2000goldobin@mail.ru>
2021-11-03 01:35:36 +0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,12 @@ import Footer from './Footer'
 import '../style/Main.css'
 
 const games = [
-	{ name: "Tic-Tac-Toe", img: "./tictactoe.svg", to: "/xox" },
+	{ name: "Tic-Tac-Toe", img: "./tictactoe.svg", to: "/xox", color: "orange" },
 	//{ name: "2048", img: "./2048.svg", to: "/" },
 	//{ name: "Minesweeper", img: "./soon.jpg", to: "/" },
 	//{name:"Developing...", img:"./soon.jpg", to: "/"},
-	{ name: "Find a couple", img: "./soon.jpg", to: "/pairs" },
-	{ name: "Rock, paper, scissors!", img: "./soon.jpg", to: "/rps" }
+	{ name: "Find a couple", img: "./pairs.svg", to: "/pairs", color: "green" },
+	{ name: "Rock, paper, scissors!", img: "./tictactoe.svg", to: "/rps", color: "blue" }
 ]
 
 const Main = () => {
@@ -21,7 +21,7 @@ const Main = () => {
 			<Header />
 			<div className="main__menu">
 				{
-					games.map((game) => (<GameCard name={game.name} img={game.img} key={game.name} to={game.to} />))
+					games.map((game) => (<GameCard name={game.name} img={game.img} key={game.name} to={game.to} color={game.color} />))
 				}
 			</div>
 			<Footer />
