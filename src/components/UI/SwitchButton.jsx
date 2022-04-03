c6758c884f188fd07ef8367bce9c0878d87a0c9b
MGoldobin <misha2000goldobin@mail.ru>
2022-04-03 17:45:12 +0500

    add SwitchButton component

---
+++ src/components/UI/SwitchButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import Switch from '@mui/material/Switch'
+import { observer } from 'mobx-react-lite'
+import store from '../../store/theme'
+
+const StyledSwitchButton = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`
+
+const SwitchButton = observer(() => {
+	return (
+		<StyledSwitchButton>
+			<img src={store.isDarkTheme ? "./moonWhite.png" : "./moonBlack.png"} alt="Night mode" />
+			<Switch 
+				checked={store.isDarkTheme}
+				onChange={store.changeTheme} 
+			/>
+		</StyledSwitchButton>
+	)
+})
+
+export default SwitchButton
