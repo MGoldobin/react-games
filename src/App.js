17f037a615b3a08377c992e175038a13bac41e08
MGoldobin <misha2000goldobin@mail.ru>
2022-04-07 01:16:46 +0500

    switch to styled-component

--- src/App.js
+++ src/App.js
@@ -6,21 +6,37 @@ import Error from './components/Error/Error.jsx'
 import Tetris from './components/Tetris/Tetris'
 import React from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import styled from 'styled-components'
+import store from './store/theme'
+import { observer } from 'mobx-react-lite'
 
-function App() {
+const StyledApp = styled.div`
+  width: 100%;
+  heigth: 100%; 
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: ${props => props.theme.backgroundColor};
+`
+
+const App = observer(() => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/xox" component={Game} />
-        <Route path="/rps" component={RPS} />
-        <Route path="/pairs" component={Pairs} />
-        <Route path="/tetris" component={Tetris} />
-        <Route path="/404" component={Error} />
-        <Redirect to="/404" />
-      </Switch>
+      <StyledApp theme={store.theme}>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/xox" component={Game} />
+          <Route path="/rps" component={RPS} />
+          <Route path="/pairs" component={Pairs} />
+          <Route path="/tetris" component={Tetris} />
+          <Route path="/404" component={Error} />
+          <Redirect to="/404" />
+        </Switch>
+      </StyledApp>
     </BrowserRouter>
   )
-}
+})
 
 export default App
