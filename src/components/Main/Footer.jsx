26ca98e65489c5aa09a484c8595cbef3ef3fd06d
MGoldobin <misha2000goldobin@mail.ru>
2022-04-07 01:17:25 +0500

    add dark theme

--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import SwitchButton from '../UI/SwitchButton'
+import store from '../../store/theme'
+import { observer } from 'mobx-react-lite'
 
 const StyledFooter = styled.footer`
 	width: 100%;
@@ -17,15 +19,15 @@ const Link = styled.a`
 	cursor: pointer;
 `
 
-const Footer = () => {
+const Footer = observer(() => {
 	return (
 		<StyledFooter>
 			<SwitchButton />
 			<Link href="https://github.com/MGoldobin/react-games" target="blank">
-				<img src="./GitHub.png" alt="GitHub" />
+				<img src={store.isDarkTheme ? "./GitHubLight.png" : "./GitHubDark.png"} alt="GitHub" />
 			</Link>
 		</StyledFooter>
 	)
-}
+})
 
 export default Footer
