7e9f98991bb0337cf93c9a4b86c95a3816fc1f60
MGoldobin <misha2000goldobin@mail.ru>
2022-04-08 01:44:42 +0500

    init tetris component

--- src/components/Tetris/Tetris.jsx
+++ src/components/Tetris/Tetris.jsx
@@ -1,20 +1,31 @@
+import { observer } from 'mobx-react-lite'
 import React/*, { useState } */from 'react'
 //import Popup from '../UI/Popup.jsx'
 import styled from 'styled-components'
 import Header from '../UI/Header'
+import store from '../../store/theme'
 
 const GamePage = styled.div`
-	
+	min-height: 100vh;
+	width: 100vw;
+	background-color: ${props => props.theme.backgroundColor};
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	color: ${props => props.theme.color};
+	position: relative;
 `
 
-const Tetris = () => {
+const Tetris = observer(() => {
 	return (
-		<GamePage>
+		<GamePage theme={store}>
 			<Header to="/" title="Tetris"/>
 
-			<button>Начать заново</button>
+			{
+				//<button>Начать заново</button>
+			}
 		</GamePage>
 	)
-}
+})
 
 export default Tetris
