aa915591b992db709cba639a10d306c735a09596
Mike <mgoldobin@psu.ru>
2022-04-14 01:51:01 +0500

    change poup

--- src/components/UI/Popup.jsx
+++ src/components/UI/Popup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import styled, { css } from 'styled-components'
-import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import Button from './Button'
+import NavButton from './NavButton'
 
 const StyledPopup = styled.div`
   display: flex;
@@ -30,46 +31,13 @@ const Title = styled.h2`
   margin: 0 0 50px 0;
 `
 
-const Button = css`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 200px;
-  height: 40px;
-  font-size: 90%;
-  font-weight: 700;
-  text-decoration: none;
-  border: 2px solid rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-  color: #000;
-  margin: 10px 0;
-  outline: none;
-  border-radius: 3px;
-
-	&:hover {
-  background-color: rgba(0, 0, 0, 0.1);
-	}
-
-	@media screen and (max-width: 425px) {
-			width: 80%;
-	}
-`
-
-const ButtonRestart = styled.button`
-	${Button}
-`
-
-const ButtonLink = styled(NavLink)`
-	${Button}
-`
-
 const Popup = ({ result, startNewGame }) => {
 	return (
 		<StyledPopup>
 			<Body>
 				<Title>{result}</Title>
-				<ButtonRestart onClick={startNewGame}>Играть еще раз!</ButtonRestart>
-				<ButtonLink to="/">На главную</ButtonLink> {/* свомал */}
+				<Button onClick={startNewGame} title="Играть еще раз!" />
+				<NavButton to="/" title="На главную"/>
 			</Body>
 		</StyledPopup>
 	)
