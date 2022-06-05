e80663bbd23909d179bda29f1aec606c328c4f54
Mike <mgoldobin@psu.ru>
2022-06-06 01:26:00 +0500

    fix store import

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,7 +1,7 @@
 import { observer } from 'mobx-react-lite'
 import React from 'react'
 import styled from 'styled-components'
-import store from '../../store/theme'
+import store from '../store/theme'
 import Button from './Button'
 import NavButton from './NavButton'
 
