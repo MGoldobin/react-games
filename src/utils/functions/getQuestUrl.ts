5da05138f2d42e2f7d27eea4bcbb54430c4f95b3
Mike <mgoldobin@psu.ru>
2022-07-16 01:05:34 +0500

    chnage url generator function

--- src/utils/functions/getQuestUrl.ts
+++ src/utils/functions/getQuestUrl.ts
@@ -1,12 +1,12 @@
 import { QUIZ_MAIN_URL } from '../constants/constants'
 
 interface FuncProps {
-	category: string
-	difficulty: string
+	category: string|null
+	difficulty: string|null
 }
 
 export const getQuestUrl = ({category, difficulty}:FuncProps) => {
-	let url = QUIZ_MAIN_URL + `?amount=1&type=multiple`
+	let url = QUIZ_MAIN_URL + `?amount=1&type=multiple&encode=base64`
 	url = difficulty ? url + `&difficulty=${difficulty}`: url
 	url = category ? url + `&category=${category}`: url
 	return url
