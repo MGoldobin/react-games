3a0189ebb1458a5545395755123aea41891c9cc4
Mike <mgoldobin@psu.ru>
2022-07-16 01:05:52 +0500

    chnage shuffle function

--- src/utils/functions/shuffleArray.ts
+++ src/utils/functions/shuffleArray.ts
@@ -1,3 +1,7 @@
-export const shuffleArray = <T>(array: T[]): T[] => {
+export const shuffleDoubleArray = <T>(array: T[]): T[] => {
 	return array.concat(array).sort(() => Math.random() - 0.5)
+}
+
+export const shuffleArray = <T>(array: T[]): T[] => {
+	return array.sort(() => Math.random() - 0.5)
 }
