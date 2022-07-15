1c67a8b0ec37eaae328cf72606446289f10aca14
Mike <mgoldobin@psu.ru>
2022-07-16 01:04:45 +0500

    add quiz game

--- src/pages/Quiz/QuestForm.tsx
+++ src/pages/Quiz/QuestForm.tsx
@@ -1,27 +1,98 @@
-import React from 'react'
+import React, { Dispatch, useState, SetStateAction } from 'react'
+import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
+
 import { QuestionData } from '../../utils/types/QuestionData'
+import { shuffleArray } from '../../utils/functions/shuffleArray'
 
 interface QuestFormProps {
-	data: QuestionData
+	data: QuestionData,
+	setPlaying: Dispatch<SetStateAction<boolean>>
 }
 
 const StyledQuestForm = styled.div`
-	color: #fff;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	width: 80%;
 `
 
-const QuestForm = ({data}: QuestFormProps) => {
+const Question = styled.h1`
+	text-decoration: underline;
+`
+
+const AnswersGrid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	gap: 10px;
+`
+
+const Answer = styled.p`
+	border: 1px solid ${props => props.theme.borderColor};
+	height: 30px;
+	line-height: 30px;
+
+	&:hover {
+		border: 2px solid ${props => props.theme.borderColor};
+		cursor: pointer;
+	}
+`
+
+const QuestForm = observer(({data, setPlaying}: QuestFormProps) => {
+	const [findRightAnswer, setFindRightAnswer] = useState(false)
+	const [clickAnswer, setClickAnswer] = useState(false)
+	const answersArr = shuffleArray([...data.incorrect_answers, data.correct_answer])
+
+	const checkAnswer = (answer: string) => {
+		return answer === data.correct_answer
+	}
+
+	const handleAnswer = (answer: string) => {
+		setClickAnswer(true)
+		if (checkAnswer(answer)) setFindRightAnswer(true)
+	}
+
+	const border = (answer: string) => {
+		if (clickAnswer && checkAnswer(answer)) return '3px solid green'
+		if (clickAnswer && !checkAnswer(answer)) return '3px solid red'
+		return '1px solid #000'
+	}
+	
+	const final = () => {
+		if (clickAnswer) {
+			return findRightAnswer ? "Вы ответили правильно!!!" : `Правильный ответ: ${data.correct_answer}`
+		} else return ""
+	}
+
 	return (
 		<StyledQuestForm>
+			<Question>{data.question}</Question>
+			<AnswersGrid >
+				{
+					answersArr.map(answer => 
+						<Answer 
+							key={answer}
+							style={{
+								border: border(answer)
+							}}
+							onClick={() => handleAnswer(answer)}
+						>
+							{answer}
+						</Answer>
+					)
+				}
+			</AnswersGrid>
 			{
-				data.question
-				/*
-				<Question />
-				<Answers />
-				*/
+				final()
 			}
+			{
+				clickAnswer 
+				? <button onClick={() => setPlaying(false)}>Следующий вопрос</button> /*исправить баг и сразу грузить новый вопрос*/
+				: null
+			}
+			
 		</StyledQuestForm>
 	)
-}
+})
 
 export default QuestForm
--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,74 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import store from '../../store/theme'
+import React, { useEffect, useState, Dispatch } from 'react'
+import Select, { SingleValue } from 'react-select'
 import axios from 'axios'
 import styled from 'styled-components'
 import { observer } from 'mobx-react-lite'
+
+import store from '../../store/theme'
 import Header from '../../components/Header'
-import Select from '../../components/Select'
 import QuestForm from './QuestForm'
-import { QUIZ_CATEGORY, QUIZ_DIFFICULTY } from '../../utils/constants/constants'
+
 import { getQuestUrl } from '../../utils/functions/getQuestUrl'
 import { Option } from '../../utils/types/Option'
 import { QuestionData } from '../../utils/types/QuestionData'
+import { QUIZ_CATEGORY, QUIZ_DIFFICULTY } from '../../utils/constants/constants'
+import { decodeB64 } from '../../utils/functions/decodeB64'
+
 
 const GamePage = styled.div`
-  min-height: 100vh;
+  height: 100vh;
   width: 100vw;
   display: flex;
   align-items: center;
   flex-direction: column;
-  color: ${props => props.theme.color};
   position: relative;
+  color: ${props => props.theme.color.primary};
+`
+
+const Body = styled.div`
+  font-family: 'Baloo 2', cursive;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  flex-grow: 6;
+  width: 80%;
+  background-color: ${props => props.theme.backgroundColor};
+  margin: 20px 0;
+  border-radius: 5px;
+  border: 1px solid ${props => props.theme.borderColor};
 `
 
 const Filter = styled.div`
   display: flex;
   justify-content: center;
-  width: 80%;
   gap: 20px;
+  color: #000;
 `
 
 const Quiz = observer(() => {
-  const [category, setCategory] = useState<Option>({value: "", name: ""})
-  const [difficulty, setDifficulty] = useState<Option>({value: "", name: ""})
+  const [category, setCategory] = useState<Option>({value: null, label: ""})
+  const [difficulty, setDifficulty] = useState<Option>({value: null, label: ""})
+  const [playing, setPlaying] = useState(false)
+  const [url, setUrl] = useState<string>(getQuestUrl({category: category.value, difficulty: difficulty.value}))
   const [quest, setQuest] = useState<QuestionData>({ 
     category: "",
     correct_answer: "",
     incorrect_answers: [],
-    question: ""
+    question: "",
+    difficulty: ""
   })
-  const [url, setUrl] = useState<string>(getQuestUrl({category: category.value, difficulty: difficulty.value}))
 
-  const getQuest = async () => {
-    const data = await axios.get(url)
-    setQuest(data.data.results[0])
-    console.log(data.data.results[0])
+  const handleChangeFilter = (selectedOption: SingleValue<Option> | unknown, setSate: Dispatch<Option>) => {
+    setPlaying(false)
+    setUrl(getQuestUrl({category: category.value, difficulty: difficulty.value}))
+    selectedOption 
+      ? setSate({value: (selectedOption as Option).value, label: (selectedOption as Option).label}) 
+      : setSate({value: null, label: ""})
+      console.log(selectedOption)
   }
 
-  useEffect(() => {
-    getQuest()
-  }, [])
+  const handleGetQuestion = async () => {
+    setPlaying(true)
+    const data = await axios.get(url)
+    setQuest({ 
+        category: decodeB64(data.data.results[0].category),
+        correct_answer: decodeB64(data.data.results[0].correct_answer),
+        incorrect_answers: data.data.results[0].incorrect_answers.map((ans:string) => decodeB64(ans)),
+        question: decodeB64(data.data.results[0].question),
+        difficulty: decodeB64(data.data.results[0].difficulty)
+      })
+    console.log({ 
+      category: decodeB64(data.data.results[0].category),
+      correct_answer: decodeB64(data.data.results[0].correct_answer),
+      incorrect_answers: data.data.results[0].incorrect_answers.map((ans:string) => decodeB64(ans)),
+      question: decodeB64(data.data.results[0].question),
+      difficulty: decodeB64(data.data.results[0].difficulty)
+    })
+  }
 
 	return (
 		<GamePage theme={store.theme}>
       <Header to='/' title='Quiz'/>
       <Filter>
-			  <Select 
-          title="Category" 
-          options={QUIZ_CATEGORY} 
-          option={category}
-          setOption={setCategory}
+        <Select 
+          options={QUIZ_CATEGORY}
+          onChange={(newValue) => handleChangeFilter(newValue, setCategory)}
+          placeholder={'Any category'}
+          styles={{
+            control: base => ({
+              ...base,
+              width: "200px"
+            })
+          }}
+          isClearable
         />
         <Select 
-          title="Difficulty" 
-          options={QUIZ_DIFFICULTY} 
-          option={difficulty}
-          setOption={setDifficulty}
+          options={QUIZ_DIFFICULTY}
+          onChange={(newValue) => handleChangeFilter(newValue, setDifficulty)}
+          placeholder={'Any difficulty'}
+          styles={{
+            control: base => ({
+              ...base,
+              width: "200px"
+            })
+          }}
+          isClearable
         />
       </Filter>
-      <QuestForm 
-        data={quest}
-      />
+      <Body>
+        {
+          playing
+          ? <QuestForm data={quest} setPlaying={setPlaying}/>
+          : <button onClick={handleGetQuestion}>Get question</button>
+        }
+      </Body>
 		</GamePage>
 	)
 })
